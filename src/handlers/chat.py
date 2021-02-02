703e1cdc697309a79c38a396ad3face347536f76
LehaDurotar <hwick1396@gmail.com>
2021-02-02 15:52:36 +0300

    Add base group chat handlers

--- src/handlers/chat.py
+++ src/handlers/chat.py
@@ -1,1 +1,13 @@
+from aiogram import types
+
 from src.bot import dp
+
+
+@dp.message_handler(content_types=types.ContentTypes.NEW_CHAT_MEMBERS)
+async def new_chat_member(message: types.Message):
+    await message.reply("Добро пожаловать!")
+
+
+@dp.message_handler(content_types=types.ContentTypes.LEFT_CHAT_MEMBER)
+async def left_chat_member(message: types.Message):
+    await message.reply("Ты куда...")
