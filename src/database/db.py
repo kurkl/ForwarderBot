921e335dc715c139503843e32067c093e08d8bf8
LehaDurotar <hwick1396@gmail.com>
2021-02-06 08:22:55 +0300

    Create superuser on bot startup

--- src/database/db.py
+++ src/database/db.py
@@ -2,14 +2,15 @@ from loguru import logger
 from aiogram import Dispatcher
 from aiogram.utils.executor import Executor
 
-from src.settings import POSTGRES_URI
+from src.settings import TG_ME, POSTGRES_URI
 
-from .entities import db
+from .entities import User, db
 
 
 async def on_startup(dispatcher: Dispatcher):
     logger.info("Setup PostgreSQL Connection")
     await db.set_bind(POSTGRES_URI)
+    await User.create_superuser(TG_ME)
 
 
 async def on_shutdown(dispatcher: Dispatcher):
