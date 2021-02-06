6518ff203bd6aad9278281666396dad508ce81ae
LehaDurotar <hwick1396@gmail.com>
2021-02-06 10:35:01 +0300

    Add User, Chat models; create new migration

--- src/database/base.py
+++ src/database/base.py
@@ -1,6 +1,6 @@
 # Import all the models, so that Base has them before being
 # imported by Alembic
 
-from .entities import VkPostData, db
+from .entities import Chat, User, VkPostData, db
 
-__all__ = ("db", "VkPostData")
+__all__ = ("db", "VkPostData", "User", "Chat")
