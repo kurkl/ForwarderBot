d882de70f55cde1e4212b4e0e76a780c70a5fe4d
LehaDurotar <hwick1396@gmail.com>
2021-02-17 20:37:49 +0300

    Fix wrong scheduler behavior

--- src/services/vk/broadcaster.py
+++ src/services/vk/broadcaster.py
@@ -4,10 +4,10 @@ import asyncio
 from random import uniform
 from typing import Dict, List, Type, Union
 from datetime import datetime, timedelta
+from threading import Thread
 from contextlib import asynccontextmanager
 from contextvars import ContextVar
 from dataclasses import dataclass
-from asyncio.queues import Queue
 
 import uvloop
 import tenacity
@@ -17,7 +17,12 @@ from aiovk.sessions import TokenSession
 
 from src.utils import RedisPool
 
-# asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+
+
+def set_loop(loop):
+    asyncio.set_event_loop(loop)
+    loop.run_forever()
 
 
 @dataclass
@@ -28,12 +33,11 @@ class Task:
 
     task_id: int
     sleep: int
-    _end_time: datetime = None
     _timeout: datetime = None
     _status: str = "READY"
 
     def __repr__(self):
-        return f"<Task (id={self.task_id}, status={self._status})>"
+        return f"<Task (id={self.task_id}, sleep={self.sleep}m, status={self._status})>"
 
     async def execute(self, func: callable, payload=dict):
         """
@@ -48,8 +52,8 @@ class Task:
         ]
         jobs = asyncio.wait(results)
         await jobs
-        self._end_time = datetime.now()
-        self._timeout = self._end_time + timedelta(minutes=self.sleep)
+        self._timeout = datetime.now() + timedelta(minutes=self.sleep)
+        self._status = "DONE"
 
     def _get_status(self):
         if not self._timeout:
@@ -77,37 +81,18 @@ class VkBroadcaster:
     Receives data from one or multiple walls and publish it
     """
 
+    redis_uri: str
+    redis_db: int = None
     token: str = None
-    loop: asyncio.AbstractEventLoop = None
-    redis_pool: RedisPool = None
-    redis_channels: List[str] = None
     is_running: ContextVar = ContextVar("Run flag")
 
     def __post_init__(self):
-        self._tasks_queue = Queue()
+        self._tasks_queue = asyncio.Queue()
         self.is_running.set(False)
-        self.redis_channels = ["main_channel:1"]
-
-    async def init_redis(self):
-        await self.redis_pool.connect()
-
-    def __await__(self):
-        return self.init_redis().__await__()
+        self.redis_pool = RedisPool(self.redis_uri, self.redis_db)
 
     def __repr__(self):
         return f"<VkBroadcaster (is_running={self.is_running}, tasks_count={self._tasks_queue.qsize()})>"
-
-    def run(self, loop: asyncio.AbstractEventLoop = None):
-        if loop:
-            self.loop = loop
-        else:
-            self.loop = asyncio.get_event_loop()
-
-        try:
-            self.is_running.set(True)
-            self.loop.run_until_complete(self.run_task_queue())
-        finally:
-            logger.info("Shutting down")
 
     @asynccontextmanager
     async def _session_scope(self):
@@ -133,11 +118,10 @@ class VkBroadcaster:
 
         result = {"target_id": target, "items": []}
         async with self._session_scope() as session:
-            received_records = await session.wall.get(owner_id=wall_id, count=10, v=5.126)
+            received_records = await session.wall.get(owner_id=wall_id, count=2, v=5.126)
 
         for record in received_records["items"][1:]:
-            item = {}
-            item.update({"date": datetime.fromtimestamp(record["date"])})
+            item = {"date": record["date"]}
             if "text" in record:
                 item.update({"text": record["text"]})
 
@@ -149,44 +133,54 @@ class VkBroadcaster:
                     # TODO: get video, polls, voice etc
 
             result["items"].append(item)
-        result.update({"execution_date": datetime.now(), "running_time": time.time() - start_time})
-        json_result = json.dumps(result, indent=4, default=str)
-        await self.redis_pool.redis.publish_json(*self.redis_channels, json_result)
+        result.update({"execution_date": time.time(), "running_time": time.time() - start_time})
+        json_result = json.dumps(result, ensure_ascii=False).encode("utf-8")
+        await self.redis_pool.redis.lpush(key=f"user_id{target}_tasks", value=json_result)
 
-    async def run_task_queue(self):
+    async def consume_queue(self):
         """
         Launch circular task queue, tasks are executed as soon as available
         TODO: add multi queues for different execution timeout
         :return:
         """
+        await self._tasks_queue.put({"task": Task(0, -1, _status="INIT"), "payload": "None"})
+        logger.info("Run task scheduler")
         try:
-            await self.redis_pool.redis.subscribe(*self.redis_channels)
+            await self.redis_pool.connect()
+            self.is_running.set(True)
             while True:
                 next_item: Dict[str, Union[Task, Type[dict]]] = await self._tasks_queue.get()
-                # logger.info(f"{next_item}")
                 if next_item["task"].ready:
                     await next_item["task"].execute(
                         func=self.fetch_public_vk_wall, payload=next_item["payload"]
                     )
-                    await self._tasks_queue.put(next_item)
-                else:
-                    await self._tasks_queue.put(next_item)
-                await asyncio.sleep(0.5)
-        except (asyncio.CancelledError, asyncio.TimeoutError) as err:
+                    logger.info(next_item)
+                await self._tasks_queue.put(next_item)
+                await asyncio.sleep(0.00001)
+        except (asyncio.TimeoutError, asyncio.CancelledError) as err:
             logger.error(f"asyncio error: {err}")
         except Exception as err:
             logger.error(f"Another error: {err}")
+        finally:
+            logger.info("Close task scheduler")
+            await self.redis_pool.disconnect()
 
     def add_task(self, task_id: int, sleep: int, targets: Dict[str, List[int]]):
         self._tasks_queue.put_nowait({"task": Task(task_id, sleep), "payload": targets})
         logger.info(f"Task {task_id} has been added")
 
-    def remove_task(self, task: Task):
-        raise NotImplemented
-
-    def pause_task(self, task: Task, timeout: int = None):
+    def remove_task(self, task: Task, timeout: int = None):
         raise NotImplemented
 
 
-def vk_broadcaster(redis_uri: str, token: str, store: int = 1) -> VkBroadcaster:
-    return VkBroadcaster(token=token, redis_pool=RedisPool(redis_uri, store))
+def get_vk_broadcaster(redis_uri: str, token: str, redis_db: int = 1) -> VkBroadcaster:
+    """
+    Creates an VkBroadcaster instance and runs it on a separate thread
+    :return:
+    """
+    loop = asyncio.new_event_loop()
+    t = Thread(target=set_loop, args=(loop,))
+    t.start()
+    broadcaster = VkBroadcaster(redis_uri=redis_uri, redis_db=redis_db, token=token)
+    asyncio.run_coroutine_threadsafe(broadcaster.consume_queue(), loop)
+    return broadcaster
