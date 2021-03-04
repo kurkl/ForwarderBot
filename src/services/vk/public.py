24672dd396e7e033d2fa90dac83cabce1b70501a
LehaDurotar <hwick1396@gmail.com>
2021-03-04 06:10:28 +0300

    Fix errors, refactor

--- src/services/vk/public.py
+++ src/services/vk/public.py
@@ -9,43 +9,40 @@ import tenacity
 from aiovk import API, TokenSession
 from loguru import logger
 from aiogram import Dispatcher
-from aiogram.types import MediaGroup, InputMediaPhoto, InlineKeyboardMarkup
+from aiogram.types import InputMediaPhoto
 from apscheduler.job import Job
 from apscheduler.events import JobExecutionEvent
 from aiogram.utils.executor import Executor
 from aiogram.utils.exceptions import BotBlocked, ChatNotFound, ChatAdminRequired
-from aiogram.utils.callback_data import CallbackData
 from apscheduler.triggers.interval import IntervalTrigger
 
 from src.runner import bot
 from src.settings import VK_TOKEN, REDIS_URI, REDIS_DB_CACHE
 from src.utils.redis import RedisPool
-from src.utils.keyboards import Constructor
 from src.utils.scheduler import scheduler
 
-vk_scheduler_cb = CallbackData("state", "id", "action")
 
-confirm_kb = Constructor.create_inline_kb(
-    [{"text": "Ок", "cb": ({"id": -1, "action": "confirm"}, vk_scheduler_cb)}], [1]
-)
-
-
-def make_respond_kb(target_id) -> InlineKeyboardMarkup:
-    return Constructor.create_inline_kb(
-        [
-            {"text": "Опубликовать сейчас", "cb": ({"id": target_id, "action": "now"}, vk_scheduler_cb)},
-            {"text": "Таймаут", "cb": ({"id": target_id, "action": "timeout"}, vk_scheduler_cb)},
-            {"text": "Авто репосты", "cb": ({"id": target_id, "action": "auto"}, vk_scheduler_cb)},
-        ],
-        [1, 1, 1],
-    )
+async def execute_event(event_data: dict):
+    if event_data["delivery"] == "auto":
+        for msg in event_data["items"]:
+            await asyncio.wait(
+                [
+                    TelegramSender.send(event_data["to_chat_id"], msg),
+                    bot.send_chat_action(event_data["to_chat_id"], "typing"),
+                ]
+            )
+    elif event_data["delivery"] == "timeout":
+        pass
 
 
 class TelegramSender:
     @classmethod
-    def prepare_message(cls, msg_data: dict) -> List[InputMediaPhoto]:
+    def prepare_message(cls, msg_data: dict) -> Union[str, List[InputMediaPhoto]]:
         result = []
         media, caption = msg_data["media"], msg_data["text"]
+
+        if not media["photos"] and not media["videos"] and not media["audio"] and not media["polls"]:
+            return caption
 
         if "photos" in media:
             for photo in media["photos"]:
@@ -56,13 +53,13 @@ class TelegramSender:
         return result
 
     @classmethod
-    async def send(cls, chat_id: int, message: Union[str, dict]):
+    async def send(cls, chat_id: int, raw_message: dict):
         try:
+            message = cls.prepare_message(raw_message)
             if isinstance(message, str):
-                await bot.send_message(chat_id=chat_id, message=message)
+                await bot.send_message(chat_id=chat_id, text=message)
             else:
-                message_with_media = cls.prepare_message(message)
-                await bot.send_media_group(chat_id=chat_id, media=message_with_media)
+                await bot.send_media_group(chat_id=chat_id, media=message)
         except ChatNotFound:
             logger.error(f"Chat {chat_id} not found")
         except ChatAdminRequired:
@@ -88,23 +85,26 @@ class VkFetch(RedisPool):
             await session.close()
 
     @tenacity.retry(wait=tenacity.wait_fixed(2), stop=tenacity.stop_after_attempt(2))
-    async def fetch_public_wall(self, user_id: int, wall_id: int, target_id: int, count):
+    async def fetch_public_wall(self, user_id: int, wall_id: int, to_chat_id: int, count):
         """
         FIXME:
         Get public vk data with wall.get method
         :param count:
         :param user_id: telegram user_id
         :param wall_id: vkontakte wall_id
-        :param target_id: telegram id channel, group or user id
+        :param to_chat_id: telegram id channel, group or user id
         :return:
         """
 
-        cache_name, user_store = f"{user_id}:cache", f"{wall_id}:{target_id}"
-        redis_data = await self.redis.hget(cache_name, user_store, encoding="utf-8")
-        fetch_result = {"target_id": target_id, "items": []}
-        cache_data = json.loads(redis_data) if redis_data else {"delivery": "", "items": []}
-        fetch_result.update({"delivery": cache_data["delivery"]})
-        await asyncio.sleep(uniform(0.150, 0.350))
+        cache_name, user_store = f"{user_id}:cache", f"{wall_id}:{to_chat_id}"
+        redis_cache_data = await self.redis.hget(cache_name, user_store, encoding="utf-8")
+        cache_data = json.loads(redis_cache_data)
+        fetch_result = {
+            "to_chat_id": cache_data["to_chat_id"],
+            "delivery": cache_data["delivery"],
+            "items": [],
+        }
+        # await asyncio.sleep(uniform(0.150, 0.350))
 
         async with self._session() as session:
             received_records = await session.wall.get(owner_id=wall_id, count=count, v=5.126)
@@ -126,18 +126,9 @@ class VkFetch(RedisPool):
             await self.redis.hset(
                 cache_name, user_store, json.dumps(fetch_result, ensure_ascii=False).encode("utf-8")
             )
-            if fetch_result["delivery"] == "auto":
-                for msg in fetch_result["items"]:
-                    await TelegramSender.send(target_id, msg)
-            else:
-                await bot.send_message(
-                    user_id,
-                    f"wall_id {wall_id}\nРепост в telegram чат {target_id}\n"
-                    f"Получено {len(fetch_result['items'])} новых постов, ваши действия?",
-                    reply_markup=make_respond_kb(wall_id),
-                )
+            await execute_event(fetch_result)
         else:
-            logger.warning("No data")
+            logger.warning(f"wall_id: {wall_id} no new post")
 
 
 class VkScheduler(VkFetch):
@@ -162,8 +153,8 @@ class VkScheduler(VkFetch):
         redis_data = await self.redis.hget(f"{args[0]}:cache", f"{args[1]}:{args[2]}", encoding="utf-8")
         return json.loads(redis_data) if redis_data else None
 
-    def add_job(self, user_id: int, wall_id: int, target_id: int, timeout: int, fetch_count: int):
-        job = self._get_job_by_id(user_id, wall_id, target_id)
+    def add_job(self, user_id: int, wall_id: int, to_chat_id: int, timeout: int, fetch_count: int):
+        job = self._get_job_by_id(user_id, wall_id, to_chat_id)
         if not job:
             scheduler.add_job(
                 self.fetch_public_wall,
@@ -171,49 +162,49 @@ class VkScheduler(VkFetch):
                 args=(
                     user_id,
                     wall_id,
-                    target_id,
+                    to_chat_id,
                     fetch_count,
                 ),
-                id=f"{user_id}:{wall_id}:{target_id}",
+                id=f"{user_id}:{wall_id}:{to_chat_id}",
                 next_run_time=datetime.now(),
             )
         else:
             logger.warning("The task is already in progress")
 
-    async def update_delivery_settings(self, user_id: int, wall_id: int, target_id: int, param: str):
-        latest_data = await self._get_data_from_redis(user_id, wall_id, target_id)
-        latest_data.update({"delivery": param})
-        await self.redis.hset(
-            f"{user_id}:cache",
-            f"{wall_id}:{target_id}",
-            json.dumps(latest_data, ensure_ascii=False).encode("utf-8"),
-        )
+    async def update_delivery_settings(self, user_id: int, wall_id: int, to_chat_id: int, param: str):
+        latest_data = await self._get_data_from_redis(user_id, wall_id, to_chat_id)
+        if not latest_data:
+            new_data = {"to_chat_id": to_chat_id, "delivery": param, "items": []}
+            await self.redis.hset(
+                f"{user_id}:cache",
+                f"{wall_id}:{to_chat_id}",
+                json.dumps(new_data, ensure_ascii=False).encode("utf-8"),
+            )
+        else:
+            latest_data.update({"delivery": param})
+            await self.redis.hset(
+                f"{user_id}:cache",
+                f"{wall_id}:{to_chat_id}",
+                json.dumps(latest_data, ensure_ascii=False).encode("utf-8"),
+            )
 
-        if param in ["now", "auto"]:
-            for msg in latest_data["items"]:
-                await asyncio.wait(
-                    [TelegramSender.send(target_id, msg), bot.send_chat_action(user_id, "typing")]
-                )
-            if param == "now":
-                await bot.send_message(user_id, "Готово, ждем новых обновлений", reply_markup=confirm_kb)
-
-    async def remove_job(self, user_id: int, wall_id: int, target_id: int):
-        job = self._get_job_by_id(user_id, wall_id, target_id)
+    async def remove_job(self, user_id: int, wall_id: int, to_chat_id: int):
+        job = self._get_job_by_id(user_id, wall_id, to_chat_id)
         if job:
             scheduler.remove_job(job.id)
-            await self.redis.hdel(f"{user_id}:cache", f"{wall_id}:{target_id}")
+            await self.redis.hdel(f"{user_id}:cache", f"{wall_id}:{to_chat_id}")
         else:
             logger.warning("No such task")
 
-    def pause_job(self, user_id: int, wall_id: int, target_id: int):
-        job = self._get_job_by_id(user_id, wall_id, target_id)
+    def pause_job(self, user_id: int, wall_id: int, to_chat_id: int):
+        job = self._get_job_by_id(user_id, wall_id, to_chat_id)
         if job:
             scheduler.pause_job(job.id)
         else:
             logger.warning("No such task")
 
-    def continue_job(self, user_id: int, wall_id: int, target_id: int):
-        job = self._get_job_by_id(user_id, wall_id, target_id)
+    def continue_job(self, user_id: int, wall_id: int, to_chat_id: int):
+        job = self._get_job_by_id(user_id, wall_id, to_chat_id)
         if job:
             scheduler.resume_job(job.id)
         else:
