5361caae84a70fb46d202cf5fc1af6fa172359ac
LehaDurotar <hwick1396@gmail.com>
2021-03-05 02:30:28 +0300

    Add active subscriber filter

--- src/filters/__init__.py
+++ src/filters/__init__.py
@@ -5,5 +5,7 @@ from aiogram import Dispatcher
 def filters_setup(dispatcher: Dispatcher):
     logger.info("Configure filters")
     from .superuser import IsSuperUserFilter
+    from .subscriber import IsActiveSubscriberFilter
 
     dispatcher.filters_factory.bind(IsSuperUserFilter)
+    dispatcher.filters_factory.bind(IsActiveSubscriberFilter)
